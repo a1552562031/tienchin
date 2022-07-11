a46da83e684061e12668a4fe5cf601c00ab8b7d2
zjh <1552562031@qq.com>
2022-07-11 19:46:24 +0800

    {build tienchin,rename workspace}

---
+++ tienchin-admin/src/main/java/com/zjh/tienchin/web/controller/tool/SwaggerController.java
@@ -0,0 +1,22 @@
+package com.zjh.tienchin.web.controller.tool;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.zjh.tienchin.common.core.controller.BaseController;
+
+/**
+ * swagger 接口
+ *
+ * @author tienchin
+ */
+@Controller
+@RequestMapping("/tool/swagger")
+public class SwaggerController extends BaseController {
+    @PreAuthorize("@ss.hasPermi('tool:swagger:view')")
+    @GetMapping()
+    public String index() {
+        return redirect("/swagger-ui.html");
+    }
+}
